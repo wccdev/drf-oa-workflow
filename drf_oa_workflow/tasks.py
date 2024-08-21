31b4ae0b27df90a92b8f5e4760266f32a9affeff
Leslie <leslie_chan@wochacha.com>
2024-08-21 11:20:23 +0800

    fix: update

--- drf_oa_workflow/tasks.py
+++ drf_oa_workflow/tasks.py
@@ -5,6 +5,7 @@ except ModuleNotFoundError:
 
 from drf_oa_workflow.models import HRMResource
 from drf_oa_workflow.models import OaUserInfo
+from drf_oa_workflow.utils import get_sync_oa_user_model
 
 
 @shared_task(name="drf_oa_workflow:同步OA用户")
@@ -12,6 +13,12 @@ def sync_oa_users():
     """
     同步Oa用户
     """
+    if get_sync_oa_user_model() != OaUserInfo:
+        return (
+            "此方法仅在使用默认OA用户信息模型drf_oa_workflow.models.OaUserInfo有效，"
+            "\n系统已重新定义OA用户信息模型，为防止任务执行错误，请重新定义同步任务。"
+        )
+
     all_oa_users = HRMResource.objects.select_related("DEPARTMENTID").all()
     objs = [
         OaUserInfo(
@@ -29,3 +36,4 @@ def sync_oa_users():
         update_fields=["staff_code_id", "dept_id", "name", "dept_name"],
         unique_fields=["user_id"],
     )
+    return None
